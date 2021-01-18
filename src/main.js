239f9d30c9a040dae700979319fcdb187ae05d31
Androz2091 <androz2091@gmail.com>
2021-01-18 13:47:00 +0100

    ⏪ Revert fontawessome addition
    This reverts commit aa3db1b706dd79bad47e5c37e1e91d65e35f4aa3.

--- src/main.js
+++ src/main.js
@@ -2,9 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
 import router from './router'
-import { FontAwesomeIcon } from "./plugins/font-awesome"
 
 createApp(App)
     .use(router)
-    .component("fa", FontAwesomeIcon)
     .mount('#app');
--- src/plugins/font-awesome.js
+++
@@ -1,9 +0,0 @@
-import { library } from "@fortawesome/fontawesome-svg-core";
-import { fas } from "@fortawesome/free-solid-svg-icons";
-import FontAwesomeIcon from "../libs/FontAwesomeIcon.vue";
-
-library.add(fas);
-
-export {
-    FontAwesomeIcon
-};
