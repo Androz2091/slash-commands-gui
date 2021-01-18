416e2e64e95bc6e44f11ab55eea39529f6aef742
Androz2091 <androz2091@gmail.com>
2021-01-18 18:03:39 +0100

    :sparkles: Add tooltip

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import './assets/css/tooltip.css'
 import router from './router'
+import VTooltip from 'v-tooltip'
 
 createApp(App)
     .use(router)
+    .use(VTooltip)
     .mount('#app');
