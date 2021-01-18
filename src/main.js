c2fa266aa15386eab4d2b2ac0616fee9f701ee3f
Androz2091 <androz2091@gmail.com>
2021-01-18 12:09:48 +0100

    :sparkles: Vue router

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import router from './router'
 
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app');
