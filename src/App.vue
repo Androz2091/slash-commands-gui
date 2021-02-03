64fe5fb865a0fd6791e8f994f2a18692aab7723d
Androz2091 <androz2091@gmail.com>
2021-02-03 13:40:51 +0100

    :recycle: New Link component and footer

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,73 @@
 <template>
-    <NavigationBar />
-    <div
-        v-if="!$store.getters.logged && $route.name !== 'Settings'"
-        class="text-center mt-28"
-    >
-        You are not logged. Go to the <router-link
-            to="/settings"
-            tag="a"
-            class="hover:underline text-discord"
+    <div class="s">
+        <NavigationBar />
+        <div
+            v-if="!$store.getters.logged && $route.name !== 'Settings'"
+            class="text-center mt-28"
         >
-            settings page
-        </router-link> to start exploring Slash Commands of your bot!
-    </div>
-    <div v-else-if="loading">
-        <div class="ball-pulse smallBall vertical-center text-center">
-            <div />
-            <div />
-            <div />
+            You are not logged. Go to the <router-link
+                to="/settings"
+                tag="a"
+                class="hover:underline text-discord"
+            >
+                settings page
+            </router-link> to start exploring Slash Commands of your bot!
         </div>
+        <div v-else-if="loading">
+            <div class="ball-pulse smallBall vertical-center text-center">
+                <div />
+                <div />
+                <div />
+            </div>
+        </div>
+        <!-- TODO: Fix Extraneous non-emits event listeners (loadCommands) warning -->
+        <RouterView
+            v-else
+            @load-commands="loadCommands"
+        />
     </div>
-    <!-- TODO: Fix Extraneous non-emits event listeners (loadCommands) warning -->
-    <RouterView
-        v-else
-        @load-commands="loadCommands"
-    />
+    <footer class="footer bg-darktwo py-4">
+        <p class="ml-2">
+            Made with <svg
+                class="w-6 h-6 inline mb-1"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+            ><path
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+            /></svg> by <Link
+                to="https://github.com/Androz2091"
+                content="Androz2091"
+            /> using <Link
+                :color="false"
+                to="https://v3.vuejs.org"
+                content="Vue.js 3"
+            /> and  <Link
+                :color="false"
+                to="https://tailwindcss.com"
+                content="TailwindCSS"
+            />. The code is publicly available on <Link
+                to="https://github.com/Androz2091/slash-commands-gui"
+                content="GitHub"
+            />.
+        </p>
+    </footer>
 </template>
 
 <script>
 import { fetchCommands } from './api';
 import NavigationBar from './components/NavigationBar.vue';
+import Link from './components/Link.vue';
 
 export default {
     name: 'App',
     components: {
-        NavigationBar
+        NavigationBar,
+        Link
     },
     data () {
         return {
---
+++ src/components/Link.vue
@@ -0,0 +1,31 @@
+<template>
+    <router-link
+        tag="a"
+        :to="to"
+        class="hover:underline"
+        :class="color ? 'text-discord' : ''"
+    >
+        {{ content }}
+    </router-link>
+</template>
+
+<script>
+export default {
+    name: 'Link',
+    props: {
+        to: {
+            type: String,
+            required: true
+        },
+        content: {
+            type: String,
+            required: true
+        },
+        color: {
+            type: Boolean,
+            required: false,
+            default: true
+        }
+    }
+};
+</script>
