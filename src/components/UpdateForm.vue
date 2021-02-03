5a0166e57c1a63b4045bcf4d014678e61fc17c2d
Androz2091 <androz2091@gmail.com>
2021-02-03 13:17:24 +0100

    :recycle: Export loading animation to dedicated component

--- src/components/UpdateForm.vue
+++ src/components/UpdateForm.vue
@@ -28,27 +28,7 @@
                 :class="updateButtonClass"
                 :disabled="updateLoading"
             >
-                <svg
-                    v-if="updateLoading"
-                    class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                >
-                    <circle
-                        class="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        stroke-width="4"
-                    />
-                    <path
-                        class="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    />
-                </svg>
+                <LoadingAnimation v-if="updateLoading" />
                 Update {{ type.toLowerCase() }}
             </button>
             <button
@@ -56,27 +36,7 @@
                 :class="deleteButtonClass"
                 :disabled="deleteLoading"
             >
-                <svg
-                    v-if="deleteLoading"
-                    class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="none"
-                    viewBox="0 0 24 24"
-                >
-                    <circle
-                        class="opacity-25"
-                        cx="12"
-                        cy="12"
-                        r="10"
-                        stroke="currentColor"
-                        stroke-width="4"
-                    />
-                    <path
-                        class="opacity-75"
-                        fill="currentColor"
-                        d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                    />
-                </svg>
+                <LoadingAnimation v-if="deleteLoading" />
                 Delete {{ type.toLowerCase() }}
             </button>
         </div>
@@ -84,8 +44,13 @@
 </template>
 
 <script>
+import LoadingAnimation from './LoadingAnimation.vue';
+
 export default {
     name: 'UpdateForm',
+    components: {
+        LoadingAnimation
+    },
     props: {
         type: {
             type: String,
--- src/views/Settings.vue
+++ src/views/Settings.vue
@@ -57,27 +57,7 @@
             :class="submitButtonClass"
             :disabled="loading || (incorrectToken || incorrectGuildID || incorrectProxyURL)"
         >
-            <svg
-                v-if="loading"
-                class="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 24 24"
-            >
-                <circle
-                    class="opacity-25"
-                    cx="12"
-                    cy="12"
-                    r="10"
-                    stroke="currentColor"
-                    stroke-width="4"
-                />
-                <path
-                    class="opacity-75"
-                    fill="currentColor"
-                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-                />
-            </svg>
+            <LoadingAnimation v-if="loading" />
             Submit
         </button>
     </form>
@@ -86,9 +66,13 @@
 <script>
 import { fakePromise } from '../util/helpers';
 import { fetchApplication, fetchGuild } from '../api';
+import LoadingAnimation from '../components/LoadingAnimation.vue';
 
 export default {
     name: 'Settings',
+    components: {
+        LoadingAnimation
+    },
     emits: ['load-commands'],
     data () {
         return {
