17aec745bef679a199de762da5a1b715258576a0
Androz2091 <androz2091@gmail.com>
2021-02-12 23:05:35 +0100

    :fire: Remove useless import

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue';
 import store from './store';
 import App from './App.vue';
 import './index.css';
-import './assets/css/tooltip.css';
 import router from './router';
 import VTooltip from 'v-tooltip';
 import Toaster from "@meforma/vue-toaster";
