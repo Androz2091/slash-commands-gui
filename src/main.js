179f42b81ae7b67199f8cc61187e2356a3d13d81
Androz2091 <androz2091@gmail.com>
2021-02-17 18:26:40 +0100

    :fire: Remove v-tooltip dependency

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@ import store from './store';
 import App from './App.vue';
 import './index.css';
 import router from './router';
-import VTooltip from 'v-tooltip';
 import Toaster from "@meforma/vue-toaster";
 import longClickDirective from './directives/long-click';
 import disableMenu from './directives/disable-menu';
@@ -12,7 +11,6 @@ const longClickInstance = longClickDirective({ delay: 2000, interval: 0 });
 
 createApp(App)
     .use(router)
-    .use(VTooltip)
     .use(Toaster)
     .use(store)
     .directive('longclick', longClickInstance)
