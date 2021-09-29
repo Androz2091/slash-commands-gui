9c8a2242081e4fdfd74f45204c324de5e365de76
Pandapip1 <45835846+Pandapip1@users.noreply.github.com>
2021-09-29 08:53:03 -0400

    Fix #24 (#25)
    🐛 Use new regex for command naming

--- src/components/CreateSlashCommand.vue
+++ src/components/CreateSlashCommand.vue
@@ -101,7 +101,7 @@ export default {
             if (commandNameMinLength) return 'Name can not be shorter than 3 characters.';
             const commandNameMaxLength = this.name.length > 32;
             if (commandNameMaxLength) return 'Name can not be longer than 32 characters.';
-            const invalidCharacters = !(/^[0-9a-zA-Z_]+$/.test(this.name));
+            const invalidCharacters = !(/^[\w_-]{1,32}$/u.test(this.name));
             if (invalidCharacters) return 'Name contains invalid characters.';
             return null;
         },
